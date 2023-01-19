1a749e205c14d07c6edd3d8717ab9d2eb947b289
admin <admin@DESKTOP-QUK0TPO>
2023-01-19 12:10:59 +0530

    check out new changes

---
+++ src/test/java/practicepackage/NewChanges.java
@@ -0,0 +1,13 @@
+package practicepackage;
+
+import org.testng.annotations.Test;
+
+public class NewChanges {
+	
+	
+	@Test
+	public void newchanges() {
+		System.out.println("hii this is my modification testscript");
+	}
+
+}
