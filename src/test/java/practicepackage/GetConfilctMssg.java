474073c5651374d9e51910438d4c293ab32a29d1
admin <admin@DESKTOP-QUK0TPO>
2023-01-19 15:30:56 +0530

    hii con

---
+++ src/test/java/practicepackage/GetConfilctMssg.java
@@ -0,0 +1,12 @@
+package practicepackage;
+
+import org.testng.annotations.Test;
+
+public class GetConfilctMssg {
+	
+	@Test
+	public void getConfilctMssg() {
+		System.out.println("hii this is my conflict testscript");
+	}
+
+}
